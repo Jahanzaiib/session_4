798e4a8cbe57585f5c83262e3c943f524c47b5bb
Jahanzaiib <jahanzaibgcuf@gmail.com>
2020-10-21 11:09:43 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+
+import {Message} from './Message.js'
+import './App.css';
+
+export default function App() {
+  let [count, setcount] = useState(1);
+  let [isDay, setDay] = useState(false)
+  let [classtime, setclasstime] = useState(true)
+  return (
+    
+    <div className = {`box ${isDay ? 'daylight' : 'box'}`}
+    className1 ={`box1 ${classtime ? 'daylight2' : 'box1'}`}
+    
+    >
+      <h1>Have a Good ={isDay ? 'Morning': 'Night'}</h1>
+  <h1>Class time ={classtime ? 'yes' : 'No'}</h1>
+      <Message counter={count}/>
+      <br />
+      <button onClick={
+          ()=>setcount(++count)
+      }>
+        Update counter
+      </button>
+      <button onClick={()=>setDay(!isDay)}> update Day</button>
+      <button onClick={()=>setclasstime(!classtime)}> Is it my class time </button>
+      
+      <h1>jahanzaib</h1>
+    </div>
+   
+  );
+  
+}
+
+
---
+++ src/Message.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Message(props){
+    return(
+        <h3>Value of counter variabl is: {props.counter}</h3>
+    )
+}
